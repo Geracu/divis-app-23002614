d4bda3076abf802bc3083bea84d34134cda7d1a3
Geracu <23002614@galileo.edu>
2024-11-16 19:45:55 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DivisApp 23002614',
+  webDir: 'dist'
+};
+
+export default config;
